8c5ac0ab17fd864a303fab4729f8341e5c3ff018
Vitalii <vitalii.lohoida@corezoid.com>
2025-02-03 22:10:48 +0200

    Fix test for hillel.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://ithillel.ua",
-    defaultCommandTimeout: 5000,
+    defaultCommandTimeout: 10000,
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
     video: true,
     screenshotOnRunFailure: true,
-     retries: 2,
+    //retries: 1,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/2-advanced-examples/hillel.cy.js
+++ cypress/e2e/2-advanced-examples/hillel.cy.js
@@ -1,22 +1,32 @@
 
 describe('Робота з користувачами', () => {
-    it('Открыть сайт Hillel', () => {
-        cy.visit('/');
-      });
-    
-      it('Клик на "Курси"', () => {
-        cy.get('button[data-dropdown-trigger="coursesMenu"]').click('button[data-dropdown-trigger="coursesMenu"]');
-    });
-    
-      it('Ввод названия курса "Сертифікація" в поле "Пошук по курсам"', () => {
-        cy.get('.search-field_input').type('Сертифікація')
-      });
-    
-      it('Ожидание поиска', () => {
-        cy.wait(2000); 
-      });
-    
-      it('Проверка результата поиска по слову "Сертифікація"', () => {
-        cy.url().should('a[href="https://ithillel.ua/courses/qa-certification"]');
-      });
+  it('Открыть сайт Hillel', () => {
+    cy.visit('/');
+    cy.get('button[data-dropdown-trigger="coursesMenu"]').click();
+    cy.get('.search-field_input').type('Сертифікація', { delay: 100 });
+    cy.wait(2000);
+    cy.get('.course-label.-alt[href="https://ithillel.ua/courses/qa-certification"]').should('be.visible');
+
   });
+
+
+  // it('Клик на "Курси"', () => {
+  //   cy.visit('/');
+  //   cy.get('button[data-dropdown-trigger="coursesMenu"]').click();
+  // });
+
+  // it('Ввод названия курса "Сертифікація" в поле "Пошук по курсам"', () => {
+  //   cy.visit('/');
+  //   cy.get('.search-field_input').type('Сертифікація')
+  // });
+
+  // it('Ожидание поиска', () => {
+  //   cy.visit('/');
+  //   cy.wait(2000); 
+  // });
+
+  // it('Проверка результата поиска по слову "Сертифікація"', () => {
+  //   cy.visit('/');
+  //   cy.url().should('a[href="https://ithillel.ua/courses/qa-certification"]');
+  // });
+});
